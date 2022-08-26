ce2e753b22a5038b1ddc01318ff56ce0d948fe25
karkhi <sufyaldy@gmail.com>
2022-08-26 10:46:53 +0800

    Perulangan dengan for dan while untuk jumlah tida terbatas dan ada variabel pemberhenti english

---
+++ dasar_sintaks_perulangan_baca_buku_dengan_while_sampai_paham_english.py
@@ -0,0 +1,24 @@
+"""
+Program perulangan dengan while
+"""
+book_count=10
+print ('Ibu berkata,"Baca semua bukumu"')
+read_count = 0
+
+understood_count = 0
+print (f'Jumlah buku yang sudah dibaca dan dipahami {understood_count}')
+
+while read_count < book_count * 2 :
+    read_count = read_count + 1
+    if understood_count == 9:
+        print(f"Buku ke {understood_count + 1} belum paham")
+    else:
+        understood_count = understood_count + 1
+        print()
+
+print(f'Jumlah buku yang sudah dibaca {understood_count}')
+if understood_count == book_count:
+    print(' "Bu, semua buku sudah dibaca dan dipahami "')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. '
+         f'Budi hanya bisa memahami {understood_count} buku"')
